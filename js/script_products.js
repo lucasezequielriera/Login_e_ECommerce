1c415fb34c329c6a9554002582c5d617a0d2b8ca
Lucas Riera <lucasezequielriera@hotmail.com>
2021-03-17 13:42:13 -0300

    Al pulsar button se abre la function

--- js/script_products.js
+++ js/script_products.js
@@ -1,11 +1,17 @@
 // ELECCIÓN DE PRODUCTOS Y ENVÍO A CARRITO DE COMPRAS // (Simulando que se clickea en botón de algún producto)
+let decision;
+let respuesta;
 
-let decision = confirm("Te gustaría comprar este producto?");
+    function comprar() {
 
-if (decision == true) {
-    alert("Perfecto! Tu producto fue agregado al carrito de compras");
-    // Acá debería haber un elemento que envíe el dato al carrito de compras y el badge se ponga en "1" o en el valor de tantos productos agregó al carrito. //
-}
-else {
-    alert("No hay problema, tenemos muchos productos para vos");
+    let decision = confirm("Te gustaría comprar este producto?");
+    if (decision == true) {
+        respuesta = "Perfecto! Tu producto fue agregado al carrito de compras";
+        // Acá debería haber un elemento que envíe el dato al carrito de compras y el badge se ponga en "1" o en el valor de tantos productos agregó al carrito. //
+    }
+    else {
+        respuesta = "No hay problema, tenemos muchos productos para vos";
+        
+    }
+    return alert(respuesta);
 }
