d9fa1cb6baf96552e575c8ac3d84339ff3dbf790
Lucas Riera <lucasezequielriera@hotmail.com>
2021-03-18 23:58:16 -0300

    Agregando Objetos (en proceso)

--- js/script_products.js
+++ js/script_products.js
@@ -2,9 +2,26 @@
 let decision;
 let respuesta;
 
+
+function Curso(nombre, precio, profesor) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.profesor = profesor;
+    return `Curso ${this.nombre}`;
+}
+
+const curso1 = new Curso("HTML", "$5000", "Ramon Diaz");
+const curso2 = new Curso("CSS", "$5000", "Ramon Diaz");
+const curso3 = new Curso("JS", "$5000", "Ramon Diaz");
+const curso4 = new Curso("ReactJS", "$5000", "Ramon Diaz");
+const curso5 = new Curso("PHP", "$5000", "Ramon Diaz");
+const curso6 = new Curso("Python", "$5000", "Ramon Diaz");
+const curso7 = new Curso("C++", "$5000", "Ramon Diaz");
+
+
     function comprar() {
 
-    let decision = confirm("Te gustaría comprar este producto?");
+    let decision = confirm(`Te gustaría comprar el producto: ${Curso(curso1.nombre)}?`);
     if (decision == true) {
         respuesta = "Perfecto! Tu producto fue agregado al carrito de compras";
         // Acá debería haber un elemento que envíe el dato al carrito de compras y el badge se ponga en "1" o en el valor de tantos productos agregó al carrito. //
